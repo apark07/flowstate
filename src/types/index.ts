50c23397c4bb5054e1b1de46b5e43f892573c89f
apark07 <andrew1207park@gmail.com>
2025-10-26 19:41:40 -0400

    First commit

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export interface User {
+  id: string;
+  email: string;
+  name?: string;
+}
+
+export interface Exercise {
+  id: string;
+  name: string;
+  type: string;
+  muscle: string;
+  equipment: string;
+  difficulty: string;
+  instructions: string;
+}
+
+export interface WorkoutLog {
+  id: string;
+  userId: string;
+  exerciseId: string;
+  sets: number;
+  reps: number;
+  weight?: number;
+  date: string;
+}
