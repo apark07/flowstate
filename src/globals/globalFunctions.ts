36d3fe7b3bd85604062a00bf545006acbc38b6ce
AliKhachab <a.khachab620@gmail.com>
2025-11-06 20:41:34 -0500

    finished login/register feature

--- src/globals/globalFunctions.ts
+++ src/globals/globalFunctions.ts
@@ -1,9 +1,1 @@
-import { auth } from '../../FirebaseConfig.ts';
-import { useNavigate } from 'react-router-dom';
-
-const navigate = useNavigate();
-
-export const handleLogout = () => {
-    auth.signOut();
-    navigate('/');
-};
+// write global functions here
