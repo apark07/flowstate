c5dd69784811504e4a1699e87dda442a0782b074
AliKhachab <a.khachab620@gmail.com>
2025-12-11 18:15:20 -0500

    minor styling changes to make it look better on the landing page

--- src/components/LandingPageCard.tsx
+++ src/components/LandingPageCard.tsx
@@ -3,17 +3,21 @@ interface LandingPageCardProps {
   text: string;
   subtext: string;
 }
-    
-export default function LandingPageCard({image, text, subtext}: LandingPageCardProps) {
+
+export default function LandingPageCard({
+  image,
+  text,
+  subtext,
+}: LandingPageCardProps) {
   return (
-<div className="flex flex-col rounded-lg overflow-hidden border border-gray-100">
-              <div className="h-80 w-full">
-                <img src={image} alt={text} className="w-full h-full object-cover" />
-              </div>
-              <div className="p-4">
-                <h3 className="text-lg font-medium text-gray-900">{text}</h3>
-                <p className="mt-2 text-sm text-gray-600">{subtext}</p>
-              </div>
+    <div className="flex flex-col rounded-lg overflow-hidden border border-gray-100 bg-blue-50">
+      <div className="h-80 w-ful">
+        <img src={image} alt={text} className="w-full h-full object-cover" />
+      </div>
+      <div className="p-4">
+        <h3 className="text-lg font-bold text-gray-900">{text}</h3>
+        <p className="mt-2 text-sm text-gray-700">{subtext}</p>
+      </div>
     </div>
-  )
+  );
 }
